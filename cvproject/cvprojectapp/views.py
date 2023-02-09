84a67b1b27a91aadd7f69923881fc910c8ac7057
arunbhalli <tomarbhalli@gmail.com>
2023-02-09 14:55:17 +0100

    madds form page cv generator

--- cvproject/cvprojectapp/views.py
+++ cvproject/cvprojectapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-
+from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 
+def accept(request):
+  return render(request, '../templates/cvform.html')
